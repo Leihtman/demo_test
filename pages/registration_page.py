a39ab5df6c1d6f4dc40356843e80d88d5249b75e
Leihtman <kekia@yandex.ru>
2024-06-13 22:30:05 +0300

    test part

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -6,8 +6,13 @@ settings = get_settings()
 
 
 class RegistrationPage(BasePage):
-    username = PageElement(id_='username')
-    password = PageElement(name='password')
+    first_name = PageElement(id_='firstName')
+    last_name = PageElement(id_='lastName')
+    user_email = PageElement(id_='userEmail')
+    male_radio_button = PageElement(xpath='//input[@name=\'gender\'][@type=\'radio\'][@value=\'Male\']')
+    female_radio_button = PageElement(xpath='//input[@name=\'gender\'][@type=\'radio\'][@value=\'Female\']')
+    other_radio_button = PageElement(xpath='//input[@name=\'gender\'][@type=\'radio\'][@value=\'Other\']')
+    user_number = PageElement(id_='userNumber')
     login = PageElement(css='input[type="submit"]')
 
     def __init__(self, webdriver):
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,3 +1,5 @@
+from selenium.webdriver import Keys
+
 from config.settings import get_settings
 from pages.registration_page import RegistrationPage
 
@@ -7,4 +9,9 @@ settings = get_settings()
 def test_success_registration(browser):
     registration_page = RegistrationPage(browser)
     registration_page.open_page()
+    registration_page.first_name.send_keys("John")
+    registration_page.last_name.send_keys("Doe")
+    registration_page.user_email.send_keys("test@gmail.com")
+    registration_page.male_radio_button.send_keys(Keys.SPACE)
+    registration_page.user_number.send_keys(89991232211)
     pass
