123a0f7959319e311a3537bd0a1bcb0ef59275af
leihtman <kekia@yandex.ru>
2024-06-14 12:47:42 +0300

    actualized

--- conftest.py
+++ conftest.py
@@ -11,8 +11,7 @@ def pytest_addoption(parser):
 
 
 def pytest_configure(config):
-    env_file_name = config.getoption("--env_file")
-    os.environ["env_file_name"] = env_file_name
+    os.environ["env_file_name"] = config.getoption("--env_file")
 
 
 @pytest.fixture
